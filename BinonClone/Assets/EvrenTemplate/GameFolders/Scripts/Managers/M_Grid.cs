d3a0a271e61b628002dd5e92699d0a33fad74a29
DESKTOP-CJLODPR\Evren <EvrenOzgur1993@gmail.com>
2022-06-18 13:46:56 +0300

    Grid Oluşturuldu.

--- BinonClone/Assets/EvrenTemplate/GameFolders/Scripts/Managers/M_Grid.cs
+++ BinonClone/Assets/EvrenTemplate/GameFolders/Scripts/Managers/M_Grid.cs
@@ -1,18 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class M_Grid : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // --------- Grid ----------
+    public GridItem GridItemPrefab;
+
+    [HideInInspector] public int GridLenghtI = 10;
+    [HideInInspector] public int GridLenghtJ = 10;
+    GridItem[,] GridArray;
+
+    private void OnEnable()
     {
-        
+        M_Observer.OnGameCreate += GameCreate;
+    }
+    private void OnDisable()
+    {
+        M_Observer.OnGameCreate -= GameCreate;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void GameCreate()
     {
-        
+        GridCreate();
+    }
+
+    private void GridCreate()
+    {
+        GridArray = new GridItem[GridLenghtI, GridLenghtJ];
+        for (int i = 0; i < GridLenghtI; i++)
+        {
+            for (int j = 0; j < GridLenghtJ; j++)
+            {
+                GridItem _gridItem = Instantiate(GridItemPrefab , transform);
+                _gridItem.transform.position = new Vector3(i,j,0);
+                _gridItem.IndexI = i;
+                _gridItem.IndexJ = j;
+                GridArray[i, j] = _gridItem;
+            }
+        }
     }
 }
